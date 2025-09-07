ed46c1684a0bb0ba3ae89b6ddf63b9b924c9bc74
vamshisangireddy <vamshisangireddy5@gmail.com>
2025-09-06 23:56:09 -0500

    k

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "aws_region" {
   description = "AWS region for infrastructure"
-  type        = string
+  type = string
 }
 
 variable "aws_key_name" {
   description = "Name of the AWS key pair to use for EC2 instances"
-  type        = string
+  type = string
 }
